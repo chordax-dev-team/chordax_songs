64f61c4cd1915fa694e4c70af0ad537668cdf60d
sb17018 <piotr.a.bar@gmail.com>
2024-11-15 23:39:13 +0100

    Initialize project

---
+++ chordax_songs/src/main/java/chordax_dev_team/chordax_songs/ChordaxSongsApplication.java
@@ -0,0 +1,13 @@
+package chordax_dev_team.chordax_songs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChordaxSongsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChordaxSongsApplication.class, args);
+	}
+
+}
---
+++ chordax_songs/src/test/java/chordax_dev_team/chordax_songs/ChordaxSongsApplicationTests.java
@@ -0,0 +1,13 @@
+package chordax_dev_team.chordax_songs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChordaxSongsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
