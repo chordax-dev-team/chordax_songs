43fa2d930aba664f53c0bddbfef7033b013216c8
piotr.bar <piotr.bar@accenture.com>
2025-09-16 00:03:39 +0100

    Remove @EurekaEnableDiscoveryClient as redundant not to confuse that it's needed

--- chordax_songs/src/main/java/chordax_dev_team/chordax_songs/ChordaxSongsApplication.java
+++ chordax_songs/src/main/java/chordax_dev_team/chordax_songs/ChordaxSongsApplication.java
@@ -2,9 +2,7 @@ package chordax_dev_team.chordax_songs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
 @SpringBootApplication
 public class ChordaxSongsApplication {
 
